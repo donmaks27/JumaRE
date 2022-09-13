e6dbb98c7e460e8f328caaad3c51eacbc5b00a36
donmaks <m1a9k9s6@gmail.com>
2022-09-13 09:45:18 +0100

    CreateRenderEngine function

--- include/JumaRE/RenderEngine.h
+++ include/JumaRE/RenderEngine.h
@@ -117,4 +117,16 @@ namespace JumaRenderEngine
 
     template<RenderAPI API>
     RenderEngine* CreateRenderEngine() { return nullptr; }
+    inline RenderEngine* CreateRenderEngine(const RenderAPI api)
+    {
+        switch (api)
+        {
+        case RenderAPI::Vulkan: return CreateRenderEngine<RenderAPI::Vulkan>();
+        case RenderAPI::OpenGL: return CreateRenderEngine<RenderAPI::OpenGL>();
+        case RenderAPI::DirectX11: return CreateRenderEngine<RenderAPI::DirectX11>();
+        case RenderAPI::DirectX12: return CreateRenderEngine<RenderAPI::DirectX12>();
+        default: ;
+        }
+        return nullptr;
+    }
 }
